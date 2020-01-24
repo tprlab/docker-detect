c028669e7668d468b3069530d385dcea2551f79a
tprlab <dev@tprlab.com>
2020-01-24 10:02:29 -0500

    fixed paths

--- detect-app/dnn_conf.py
+++ detect-app/dnn_conf.py
@@ -1,7 +1,9 @@
-DNN_PATH = "/ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb"
-DNN_TXT_PATH = "/detect-app/opencv-extra/ssd_mobilenet_v1_coco.pbtxt"
-DNN_LABELS_PATH = "/detect-app/opencv-extra/mscoco_label_map.pbtxt"
+APP_PATH = "/docker-detect-master/detect-app"
 
-LOG_PATH = "/detect-app/logs"
+DNN_PATH = "/ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb"
+DNN_TXT_PATH = APP_PATH + "/opencv-extra/ssd_mobilenet_v1_coco.pbtxt"
+DNN_LABELS_PATH = APP_PATH + "/opencv-extra/mscoco_label_map.pbtxt"
+
+LOG_PATH = APP_PATH + "/logs"
 LOG_FILE = "dnn_rest.log"
 
