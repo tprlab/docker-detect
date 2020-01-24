e77c6b534eb8a44ba110ac20600a3dd3a1091eaa
tprlab <dev@tprlab.com>
2020-01-24 09:56:10 -0500

    Small fixes

--- detect-app/dnn_conf.py
+++ detect-app/dnn_conf.py
@@ -1,7 +1,7 @@
 DNN_PATH = "/ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb"
-DNN_TXT_PATH = "/detect/opencv-extra/ssd_mobilenet_v1_coco.pbtxt"
-DNN_LABELS_PATH = "/detect/opencv-extra/mscoco_label_map.pbtxt"
+DNN_TXT_PATH = "/detect-app/opencv-extra/ssd_mobilenet_v1_coco.pbtxt"
+DNN_LABELS_PATH = "/detect-app/opencv-extra/mscoco_label_map.pbtxt"
 
-LOG_PATH = "/detect/logs"
+LOG_PATH = "/detect-app/logs"
 LOG_FILE = "dnn_rest.log"
 
